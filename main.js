8b282bdeb35355a3285036e92edacc53f2fc7774
Israel Huacce <israxd7u7@gmail.com>
2023-11-03 10:52:35 -0500

    add variables and js, add font family

---
+++ main.js
@@ -0,0 +1,11 @@
+let timeFocus = 26.95
+let timeGuitar = 6.65
+let timeProgramming = 18
+let readBooks = 50
+let readManga = 4
+
+document.getElementById("time-focus").textContent = timeFocus;
+document.getElementById("time-guitar").textContent = timeGuitar;
+document.getElementById("time-programming").textContent = timeProgramming;
+document.getElementById("read-books").textContent = readBooks;
+document.getElementById("read-manga").textContent = readManga;
