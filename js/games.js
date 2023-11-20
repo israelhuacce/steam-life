08234bd0ff74abbb00a7bfa596f4973ad4ff1bcb
Israel Huacce <israxd7u7@gmail.com>
2023-11-19 21:29:42 -0500

    add page juegos, articulos, change covers of areas, release 19-11-2023

---
+++ js/games.js
@@ -0,0 +1,20 @@
+let dota2time = 5660.4
+let eurotruck = 204.3
+let csgo2 = 164.9
+let lol = 60
+let genshin = 50
+let lf4d2 = 44.5
+let americantruck = 37.1
+let gtav = 30.6
+let halflife = 11.2
+
+
+document.getElementById("time-dota2").textContent = dota2time;
+document.getElementById("time-eurotruck").textContent = eurotruck;
+document.getElementById("time-csgo2").textContent = csgo2;
+document.getElementById("time-lol").textContent = lol;
+document.getElementById("time-genshin").textContent = genshin;
+document.getElementById("time-lf4d2").textContent = lf4d2;
+document.getElementById("time-americantruck").textContent = americantruck;
+document.getElementById("time-gtav").textContent = gtav;
+document.getElementById("time-halflife").textContent = halflife;
---
+++ js/main.js
@@ -0,0 +1,12 @@
+let timeFocus = 33.09
+let timeGuitar = 14.65
+let timeProgramming = 23.1
+let readBooks = 50
+let readManga = 4
+let numeroTocadas = 3
+
+document.getElementById("time-focus").textContent = timeFocus;
+document.getElementById("time-guitar").textContent = timeGuitar;
+document.getElementById("time-programming").textContent = timeProgramming;
+document.getElementById("read-books").textContent = readBooks;
+document.getElementById("read-manga").textContent = readManga;
